aec34404caac8df1ffec0e38a1b753a15a8af0a3
leosgarbi <lsgarbi1@gmail.com>
2024-05-09 18:33:15 -0300

    feat: passing component information by context

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,26 +1,11 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import * as zod from 'zod';
+import { useContext } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { CyclesContext } from '../..';
 import { FormContainer, MinutesAmountInput, TaskInput } from './setyled';
 
-const newCycleFormValitadionSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod
-    .number()
-    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
-    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
-});
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValitadionSchema>;
-
 export function NewCycleForm() {
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValitadionSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  });
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
 
   return (
     <FormContainer>
